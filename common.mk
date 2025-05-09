114c2db130489d9bb57fddd109aadc83c1b51085
SkyMinus <minus_rav@qq.com>
2025-05-09 11:23:35 +0800

    sm8550-common: Build XiaomiParts
    Added XiaomiParts to PRODUCT_PACKAGES

--- common.mk
+++ common.mk
@@ -90,6 +90,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.automotive.vehicle@2.0-manager-lib
 
+# Xiaomi Parts
+PRODUCT_PACKAGES += \
+    XiaomiParts
+
 # Atrace
 PRODUCT_PACKAGES += \
     android.hardware.atrace@1.0-service
