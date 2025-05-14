AnranYus/android_device_xiaomi_sm8550-common

sm8550-common: Add support for vermeer

Android.mk
@@ -6,7 +6,7 @@
 
 LOCAL_PATH := $(call my-dir)
 
-ifneq ($(filter fuxi nuwa socrates,$(TARGET_DEVICE)),)
+ifneq ($(filter fuxi nuwa socrates vermeer,$(TARGET_DEVICE)),)
 include $(LOCAL_PATH)/vendor-symlinks.mk
 include $(call all-subdir-makefiles,$(LOCAL_PATH))
 

MinusOpenSource/device_xiaomi_sm8550-common

Android.mk
@@ -6,6 +6,6 @@
 
 LOCAL_PATH := $(call my-dir)
 
-ifneq ($(filter nuwa fuxi socrates,$(TARGET_DEVICE)),)
+ifneq ($(filter vermeer nuwa fuxi socrates,$(TARGET_DEVICE)),)
 include $(call all-subdir-makefiles,$(LOCAL_PATH))
 endif