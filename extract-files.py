0ffacf3611ad196396e02e6f9dfa0939e1726582
SkyMinus <minus_rav@qq.com>
2025-05-21 10:20:04 +0800

    sm8550-common: Remove unnecessary libbase_shim blob fixup

--- extract-files.py
+++ extract-files.py
@@ -94,11 +94,6 @@ blob_fixups: blob_fixups_user_type = {
         'vendor/etc/media_codecs_kalama_vendor.xml',
     ): blob_fixup()
         .regex_replace('.+media_codecs_(google_audio|google_c2|google_telephony|vendor_audio).+\n', ''),
-    (
-        'vendor/lib64/libstfactory-vendor.so',
-        'odm/lib64/nfc_nci.nqx.default.hw.so'
-    ): blob_fixup()
-        .add_needed('libbase_shim.so'),
     'vendor/etc/ueventd.rc' : blob_fixup()
         .add_line_if_missing('\n# Charger\n/sys/class/qcom-battery     night_charging            0660    system  system')
 }  # fmt: skip
