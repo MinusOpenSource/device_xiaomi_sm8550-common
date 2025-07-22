66d6eaad54d5128d35677ce51be5d3081b276c8f
SkyMinus <minus_rav@qq.com>
2025-07-22 23:17:21 +0800

    sm8550-common: Build telephony-ext

--- common.mk
+++ common.mk
@@ -353,12 +353,14 @@ PRODUCT_PACKAGES += \
     qti-telephony-utils \
     qti_telephony_hidl_wrapper.xml \
     qti_telephony_hidl_wrapper_prd.xml \
-    qti_telephony_utils.xml
+    qti_telephony_utils.xml \
+    telephony-ext
 
 PRODUCT_PACKAGES += \
     xiaomi-telephony-stub
 
 PRODUCT_BOOT_JARS += \
+    telephony-ext \
     xiaomi-telephony-stub
 
 PRODUCT_COPY_FILES += \
